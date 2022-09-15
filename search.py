380d9272b059282fa88ba5643bef2f1b0d833835
dpears04 <dpears04@uoguelph.ca>
2022-09-15 12:40:31 -0400

    Fixed issue with CLI not looping after first input

--- search.py
+++ search.py
@@ -1,18 +1,16 @@
 import csv
 import sys
 
-
-
-
 with open('courseData.csv', newline='') as csvfile:
 	spamreader = csv.reader(csvfile)
-	
 	course = input("(e) to exit >")
 	while(course is not 'e'):
+		
 		for row in spamreader:
 			temp = ' '.join(row)
 			if(course.upper() in temp):
 				print(temp)
 				print('\n')
+		csvfile.seek(0)
 		
 		course = input(">")
