c266ff3be56ed6c9354fd7f4bd4c932b48cf681a
dpears04 <dpears04@uoguelph.ca>
2022-09-15 12:29:20 -0400

    Fixed exit command

--- search.py
+++ search.py
@@ -7,8 +7,8 @@ import sys
 with open('courseData.csv', newline='') as csvfile:
 	spamreader = csv.reader(csvfile)
 	
-	course = input(">")
-	while(course.lower() is not 'e'):
+	course = input("(e) to exit >")
+	while(course is not 'e'):
 		for row in spamreader:
 			temp = ' '.join(row)
 			if(course.upper() in temp):
