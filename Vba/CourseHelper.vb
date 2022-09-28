f452e3d1476271fd550b07cf616948c808295bdb
Dylan Goodman <dylan.goodman60@gmail.com>
2022-09-28 16:18:44 -0400

    add courseHelper module with example sub's and func's

---
+++ Vba/CourseHelper.vb
@@ -0,0 +1,9 @@
+'Subs can change values but don't return anything
+Sub hello()
+    Debug.Print ("Hey! I'm the course helper.")
+End Sub
+
+'Functions return values
+Function getBool()
+    getBool = True
+End Function
