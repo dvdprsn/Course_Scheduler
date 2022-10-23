cf7ddc314873692081a6e14c6ec1faa470a6888c
David Pearson <dpears04@uoguelph.ca>
2022-10-23 16:30:52 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt-get install nginx
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt -subj "/C=CA/ST=Ontario/L=Guelph/O=G103/OU=CIS3760/CN='hostname -f'/emailAddress=dpears04@uoguelph.ca"
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt -subj "/C=CA/ST=Ontario/L=Guelph/O=G103/OU=CIS3760/CN=CourseSelect/emailAddress=example@gmail.com"
 openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
 cp ./course-parser-sprint-4/resources/default /etc/nginx/sites-available/
 cp ./course-parser-sprint-4/resources/self-signed.conf /etc/nginx/snippets/
